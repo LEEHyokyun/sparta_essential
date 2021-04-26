d5da8cd4a1fda38b2ed803f8f9cb1a3fa4e5036d
hyokyun <gyrbs22@gmail.com>
2021-04-26 22:38:42 +0900

    Update 01_intentions_clear.py

--- 01_week/01_intentions_clear.py
+++ 01_week/01_intentions_clear.py
@@ -1,6 +1,23 @@
+# 코드의 가독성을 높이는 방안을 고려해본다.
+# 클린코드
+
+# AS IS
+
 def get_them(the_list):
     result = []
     for x in the_list:
         if x.p1 > 19:
             result.append(x)
     return result
+
+
+# TO BE
+
+ADULT_AGE = 19
+
+def get_adults(people):
+    adults = []
+    for person in people:
+        if person.age > ADULT_AGE:
+            adults.append(person)
+    return adults
