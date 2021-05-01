5ace69eb56fb9f582e972fbd6c616dba344f4c1e
hyokyun <gyrbs22@gmail.com>
2021-05-02 01:56:49 +0900

    Create 10-3_f_string.py

---
+++ 01_week/10-3_f_string.py
@@ -0,0 +1,15 @@
+dictionary = {
+    'pencil' : 3500,
+    'pen' : 2500,
+    'notebook' : 3000,
+    5000 : 1500
+}
+
+pencil = dictionary['pencil']
+pen = dictionary['pen']
+
+string_result_pencil = f'연필의 가격은 {pencil} 입니다'
+string_result_pen = f'펜의 가격은 {pen} 입니다'
+
+print(string_result_pencil)
+print(string_result_pen)
