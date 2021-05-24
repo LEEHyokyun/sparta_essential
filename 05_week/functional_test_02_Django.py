8b82a428b303906c96afe93406fb8d734035e410
hyokyun <gyrbs22@gmail.com>
2021-05-24 23:51:06 +0900

    Delete functional_test_02_Django.py

--- 05_week/functional_test_02_Django.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-path = './chromedriver'
-driver = webdriver.Chrome(path)
-driver.get('http://localhost:8000')
-
-#assert "Django" in driver.title
-assert "install" in driver.title
