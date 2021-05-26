36b22eeba96a5a3b5759f5740c4e4bf2c03e372c
hyokyun <gyrbs22@gmail.com>
2021-05-26 23:57:05 +0900

    django MVT

--- 05_week_retry/TDD/polls/views.py
+++ 05_week_retry/TDD/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World. You are at the polls' index")
+    question_list = Question.objects.all()
+    #output = ', '.join([q.question_text for q in question_list])
+
+    context = {
+        'question_list' : question_list
+    }
+
+    return render(request, 'polls/index.html', context)
