3454f2fc997322172ea103fecd9b04f5f6e14db0
hyokyun <gyrbs22@gmail.com>
2021-05-27 23:35:04 +0900

    Add no question handling

--- 05_week_retry/TDD/polls/tests.py
+++ 05_week_retry/TDD/polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+class QuesionIndexViewTests(TestCase):
+    def test_has_no_question(self):
+        """
+        If no any questions, message will be displayed.
+        """
+        response = self.client.get("/polls/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "등록된 설문조사가 없습니다.")
+        self.assertQuerysetEqual(response.context['question_list'], [])
