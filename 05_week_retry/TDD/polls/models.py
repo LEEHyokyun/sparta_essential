dcb9a1af7c22dc0fed8bc860944566d00316480d
hyokyun <gyrbs22@gmail.com>
2021-05-29 12:21:06 +0900

    Add was_published_recently method & New mark

--- 05_week_retry/TDD/polls/models.py
+++ 05_week_retry/TDD/polls/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
 
 # Create your models here.
 class Question(models.Model):
-    #문자열 저장
-    question_text = models.CharField(max_length = 200)
-    #질문발행시간
+    # 문자열 저장
+    question_text = models.CharField(max_length=200)
+    # 질문발행시간
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length = 200)
-    #투표수
-    votes = models.IntegerField(default = 0)
+    choice_text = models.CharField(max_length=200)
+    # 투표수
+    votes = models.IntegerField(default=0)
--- 05_week_retry/TDD/polls/tests.py
+++ 05_week_retry/TDD/polls/tests.py
@@ -1,7 +1,35 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question, Choice
+import datetime
+
 
 # Create your tests here.
 class QuesionIndexViewTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False when Question date is in the future
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False when Question date is in the past, over 1 days
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True when Question date is in the past, less 1 days
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
     def test_has_no_question(self):
         """
         If no any questions, message will be displayed.
@@ -10,3 +38,19 @@ class QuesionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "등록된 설문조사가 없습니다.")
         self.assertQuerysetEqual(response.context['question_list'], [])
+
+    def test_published_recently_has_new_mark(self):
+        """
+        Questions with a recent pub_date are displayed
+        with <New> marked
+        """
+
+        recent_time = timezone.now() - datetime.timedelta(hours=8)
+        question = Question(question_text="test", pub_date=recent_time)
+        question.save()
+
+        response = self.client.get("/polls/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "[New]")
+        self.assertQuerysetEqual(response.context['question_list'], [question])
